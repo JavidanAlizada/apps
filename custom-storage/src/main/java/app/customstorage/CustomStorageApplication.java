8ba34346b916bf24bd87a61748016275b4d04c58
JavidanAlizada <javidanalizada99@gmail.com>
2021-04-22 23:27:20 +0400

    init

---
+++ custom-storage/src/main/java/app/customstorage/CustomStorageApplication.java
@@ -0,0 +1,13 @@
+package app.customstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomStorageApplication.class, args);
+	}
+
+}
---
+++ custom-storage/src/test/java/app/customstorage/CustomStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package app.customstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
